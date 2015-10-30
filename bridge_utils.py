1c3c8261bbe36ae185e8bc34390254bd8cb4a717
SwannSG <swannsg@gmail.com>
2015-10-30 17:48:04 +0200

    modified:   bridge_utils.py 	modified:   deal_cards.py

--- bridge_utils.py
+++ bridge_utils.py
@@ -63,6 +63,18 @@ def deal_distribution(deal):
         l.append(summary_of_distribution(hand))
     return tuple(l)
 
+def deal_hcp_distr(deal):
+    """
+        deal: tuple([hand_0], [hand_1], [hand_2], [hand_3])
+        return: ((hcp_C, hcp_D, hcp_H, hcp_S), (hcp_C, hcp_D, hcp_H, hcp_S), (hcp_C, hcp_D, hcp_H, hcp_S), (hcp_C, hcp_D, hcp_H, hcp_S)) 
+    """
+    l = []
+    for hand in deal:
+        l.append(hcp_distr(hand))
+    return tuple(l)
+
+    
+
 def hcp_per_suit(split):
     """
         HCP per suit
@@ -75,16 +87,18 @@ def hcp_per_suit(split):
         hcp_split.append(hcp_for_cards(each_suit))
     return tuple(hcp_split)
 
+
 def hcp_distr(cards):
     """
         return hcp per suit for a set of cards
+        (hcp_C, hcp_D, hcp_H, hcp_S)
     """
     return hcp_per_suit(split_into_suits(cards))
 
 
 def hcp_for_cards(cards):
     """
-        return high card points for a set of cards
+        return high card points for a set of cards as single integer
     """
     hcp = 0
     hcp = hcp + count_of_cards(cards, ACES)*HCP['ACE']
@@ -94,13 +108,41 @@ def hcp_for_cards(cards):
     return hcp
 
         
-
-
-
+def deal_hcp(deal):
+    """
+        deal: tuple([hand_0], [hand_1], [hand_2], [hand_3])
+        return: (hcp_1, hcp_2, hcp_3, hcp_4)
+    """
+    l = []
+    for hand in deal:
+        l.append(hcp_for_cards(hand))
+    return tuple(l)
         
+class Hand:
 
+    def __init__(self, hand):
+        """
+            hand: [hand] of integers, length 13
+        """
+        self.hand = hand
+        self.symbols = getSymbols(hand)
+        self.distr = summary_of_distribution(hand)
+        self.hcp_distr = hcp_distr(hand)
+        self.hcp = hcp_for_cards(hand)
 
-    
+    def __repr__(self):
+        #hcp, hcp_distr, distr
+        return '%s\n%s\t\t%s\t\t%s' % (str(self.symbols), self.hcp, str(self.hcp_distr), str(self.distr))
 
+class Partnership:
 
-    
+    def __init__(self, hand_1, hand_2):
+        self.hand_1 = Hand(hand_1)
+        self.hand_2 = Hand(hand_2)
+        self.hcp = self.hand_1.hcp + self.hand_2.hcp
+        self.distr = tuple([sum(x) for x in zip(self.hand_1.distr, self.hand_2.distr)])
+        self.hcp_distr = tuple([sum(x) for x in zip(self.hand_1.hcp_distr, self.hand_2.hcp_distr)])
+
+    def __repr__(self):
+        return '%s\t\t%s\t\t%s' % (self.hcp, str(self.hcp_distr), str(self.distr)) 
+
--- deal_cards.py
+++ deal_cards.py
@@ -71,11 +71,6 @@ class pattern:
         else:
             return tuple_points
         
-
-
-
-
-
 def deal():
     """
     randomly generate 4 hands
@@ -86,9 +81,12 @@ def deal():
 
 def a_wildcard_b(numeric, pattern):
     """
-        pass tuple a, b of same length = 4
-        returns a tuple of same length = 4
-            where '*' occurs
+        numeric: tuple(n1,n2,n3,n4)
+        pattern: tuple(np1||'*',np2||'*',np3||'*',np4||'*')
+        returns: (n1 or '*' if np1=='*', r2, r3, r4)
+
+        ex1: a = (3,2,5,3), b = (4,4,*,*), r = (3,2,*,*) 
+
     """
     l = []
     for index, value in enumerate(pattern):
@@ -133,6 +131,48 @@ def find_points_match(deal, pattern, integer_HCP):
                 return index
     return False
 
+def find_points_match_in_one_hand(deal, indexes, pattern, integer_HCP):
+    """
+        deal = tuple([hand_0], [hand_1], [hand_2], [hand_3])
+        pattern = (hcp_C, hcp_D, hcp_H, hcp_S)
+                    or
+        integer_HCP = '*' or integer
+        indexes = (hand_index_1, hand_index_2)
+        return = (north_hand_index, south_hand_index)
+            where pattern match is for north hand
+    """
+    if integer_HCP <> '*':
+        # ignore tuple_points, just use HCP for hand
+        if hcp_for_cards(deal[indexes[0]]) == integer_HCP:
+            return (indexes[0], indexes[1])
+        if hcp_for_cards(deal[indexes[1]]) == integer_HCP:
+            return (indexes[1], indexes[0])
+    else:
+        # use tuple_points as pattern
+#        print pattern
+#        print a_wildcard_b(hcp_distr(deal[indexes[0]]),pattern)
+#        print a_wildcard_b(hcp_distr(deal[indexes[1]]),pattern)
+#        print ''
+        if a_wildcard_b(hcp_distr(deal[indexes[0]]),pattern) == pattern:
+            return (indexes[0], indexes[1])
+        if a_wildcard_b(hcp_distr(deal[indexes[1]]),pattern) == pattern:
+            return (indexes[1], indexes[0])
+    return False
+
+def find_distribution_match_in_one_hand(deal, indexes, pattern):
+    """
+        deal = tuple([hand_0], [hand_1], [hand_2], [hand_3])
+        pattern = (nC, nD, nH, nS)
+        indexes = (hand_index_1, hand_index_2)
+        return = (north_hand_index, south_hand_index)
+            where pattern match is for north hand
+    """
+    if a_wildcard_b(summary_of_distribution(deal[indexes[0]]),pattern) == pattern:
+        return (indexes[0], indexes[1])
+    if a_wildcard_b(summary_of_distribution(deal[indexes[1]]),pattern) == pattern:
+        return (indexes[1], indexes[0])
+    return False
+
 def points_match(hand, pattern, integer_HCP):
     """
         hand: [hand]
@@ -169,6 +209,7 @@ def find_team_distribution_match(deal, pattern):
     """
         deal: tuple([hand_0], [hand_1], [hand_2], [hand_3])
         pattern: (nC, nD, nH, nS) for the team
+        return: matching indexes as tuple
     """
     a = {1:0, 2:0, 3:0, 4:1, 5:1, 6:2}
     b = {1:1, 2:2, 3:3, 4:2, 5:3, 6:3}
@@ -183,20 +224,97 @@ def find_team_distribution_match(deal, pattern):
     return False
 
 
+def find_team_hcp_match(deal_hcp, integer_HCP):
+    """
+        deal_hcp: (HCP_hand_1, HCP_hand_2, HCP_hand_3, HCP_hand_4)
+        integer_HCP: HCP for hand
+        return: matching indexes as tuple
+                or
+                False
+    """
+    a = {1:0, 2:0, 3:0, 4:1, 5:1, 6:2}
+    b = {1:1, 2:2, 3:3, 4:2, 5:3, 6:3}
+    i = 1
+    while i <= 6:
+        index_hand_1 = a[i]
+        index_hand_2 = b[i]
+        if deal_hcp[index_hand_1] + deal_hcp[index_hand_2] == integer_HCP:
+            return (index_hand_1, index_hand_2)
+        i = i + 1
+    return False
+
+def find_team_hcp_tuple_match(dealHCPdistr, pattern):
+    """
+        dealHCPdistr: tuple((hcpC,D,H,S), (hcpC,D,H,S), (hcpC,D,H,S), (hcpC,D,H,S))
+        pattern: (hcp_C, hcp_D, hcp_H, hcp_S) for team, both hands combined
+        return: matching indexes as tuple
+                or
+                False
+    """
+    a = {1:0, 2:0, 3:0, 4:1, 5:1, 6:2}
+    b = {1:1, 2:2, 3:3, 4:2, 5:3, 6:3}
+    i = 1
+    while i <= 6:
+        index_hand_1 = a[i]
+        index_hand_2 = b[i]
+        sum_hand_1_2 = tuple([sum(x) for x in zip(dealHCPdistr[index_hand_1], dealHCPdistr[index_hand_2])])
+        if a_wildcard_b(sum_hand_1_2, pattern) == pattern:
+            return (index_hand_1, index_hand_2)        
+        i = i + 1
+    return False
+
+def sum_tuple(tuple_1, tuple_2):
+    """
+        tuple_1 = (1,2,3,4)
+        tuple_2 = (6,7,8,9)
+        result = (7,9,11,13)
+    """
+    return tuple([sum(x) for x in zip(tuple_1, tuple_2)])
 
 
+def find_team_points(deal, pattern, integer_HCP):
+    """
+        deal: tuple([hand_0], [hand_1], [hand_2], [hand_3])
+        pattern: (hcp_C, hcp_D, hcp_H, hcp_S) for team, both hands combined
+        integer_HCP: HCP for hand
+    """
+    if integer_HCP <> '*':
+        # ignore tuple_points, just use HCP for both hands
+        dealHCP = deal_hcp(deal)
+        return find_team_hcp_match(dealHCP, integer_HCP)
+    else:
+        # use tuple_points as pattern
+        dealHCPdistr = deal_hcp_distr(deal)
+        return find_team_hcp_tuple_match(dealHCPdistr, pattern)
+
+def team_points_match(deal, indexes, pattern, integer_HCP):
+    """
+        deal: tuple([hand_0], [hand_1], [hand_2], [hand_3])
+        indexes: tuple(hand_x, hand_y)
+        pattern: (hcp_C, hcp_D, hcp_H, hcp_S) for team, both hands combined
+        integer_HCP: HCP for hand
+    """
+    if integer_HCP <> '*':
+        # ignore tuple_points, just use HCP for both hands
+        if hcp_for_cards(deal[indexes[0]]) + hcp_for_cards(deal[indexes[1]]) == teamHCP:
+            return True
+    else:
+        # use tuple_points as pattern
+        sum_hand_1_2 = sum_tuple(hcp_distr(deal[indexes[0]]), hcp_distr(deal[indexes[1]]))
+        if a_wildcard_b(sum_hand_1_2, pattern) == pattern:
+            return True
+    return False
 
 
-no_of_deals = 1000000
+no_of_deals = 10000000
 #p = pattern(own_points=(7, 3, 4, 6))
 #p = pattern(own_distr=(3,3,3,4),own_points=(7,3,4,6))
 #p = pattern(own_distr=(3,3,3,4),own_HCP=10)
-p = pattern(team_distr=(6,6,6,8))
-
-print 'b_own_distr: %s' % p.b_own_distr
-print 'b_own_points: %s' % p.b_own_points
-print 'b_team_distr: %s' % p.b_team_distr 
-print 'b_team_points: %s' % p.b_team_points 
+#p = pattern(team_distr=(6,6,6,8))
+#p = pattern(team_points=(5,5,5,5))
+#p = pattern(team_distr=(6,6,6,8), team_points=(5,5,5,5))
+#p = pattern(team_distr=(6,6,6,8), own_distr=(2,3,4,4))
+p = pattern(own_HCP=5, team_HCP=15)
 
 i = 1
 while i <= no_of_deals:
@@ -236,27 +354,64 @@ while i <= no_of_deals:
                 break
 
     if not p.b_own_distr and not p.b_own_points and p.b_team_distr and not p.b_team_points:
-        # team distribution
+        # team distribution only
         hand_match_indexes = find_team_distribution_match(each_deal, p.team_distr)
         if hand_match_indexes:
             # team distribution found
             N,S,W,E = NSWE(each_deal, hand_match_indexes)
             break
 
+    if not p.b_own_distr and not p.b_own_points and not p.b_team_distr and p.b_team_points:
+        # team points only
+        hand_match_indexes = find_team_points(each_deal, p.team_points, p.team_HCP)
+        if hand_match_indexes:
+            # team points found
+            N,S,W,E = NSWE(each_deal, hand_match_indexes)
+            break
+
+    if not p.b_own_distr and not p.b_own_points and p.b_team_distr and p.b_team_points:
+        # team distribution and team points
+        hand_match_indexes = find_team_distribution_match(each_deal, p.team_distr)
+        if hand_match_indexes:
+            # team distribution match found
+            if team_points_match(each_deal, hand_match_indexes, p.team_points, p.team_HCP):
+            # team points match found
+                N,S,W,E = NSWE(each_deal, hand_match_indexes)
+                break
+    if p.b_own_distr and not p.b_own_points and p.b_team_distr and not p.b_team_points:
+        # own_distr and team_distr
+        hand_match_indexes = find_team_distribution_match(each_deal, p.team_distr)
+        if hand_match_indexes:
+            # team distribution found
+            north_south_indexes = find_distribution_match_in_one_hand(each_deal, hand_match_indexes, p.own_distr)
+            if north_south_indexes:
+                # own distribution found
+                N,S,W,E = NSWE(each_deal, north_south_indexes)
+                break
+
+    if not p.b_own_distr and p.b_own_points and not p.b_team_distr and p.b_team_points:
+        # own_points and team_points
+        hand_match_indexes = find_team_points(each_deal, p.team_points, p.team_HCP)
+        if hand_match_indexes:
+            # team points found
+            north_south_indexes = find_points_match_in_one_hand(each_deal, hand_match_indexes, p.own_points, p.own_HCP)
+            if north_south_indexes:
+                # own points found
+                N,S,W,E = NSWE(each_deal, north_south_indexes)
+                break
+
+            
     i = i + 1
 
 if i > no_of_deals:
     print 'no hand found'
 else:
     print 'i: %s' % i
-    print 'hand integers: %s' % N
-    print 'hand symbols: %s' % getSymbols(N)
-    print 'summary_of_distribution N: %s' % str(summary_of_distribution(N))
-    print 'hcp_distr: %s' % str(hcp_distr(N))
-    print 'HCP: %s' % hcp_for_cards(N)
-
-    print 'summary_of_distribution S: %s' % str(summary_of_distribution(S))
-    
-
-
+    print ''
+    print 'North'
+    print Hand(N)
+    print 'South'
+    print Hand(S)
+    print 'Partnersip'
+    print Partnership(N,S)
     
